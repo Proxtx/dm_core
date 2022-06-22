153b91632ba449be68d8949a2aab02361fc7643e
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-06-22 08:52:04 +0200

    added a test impl

--- src/structs.rs
+++ src/structs.rs
@@ -1,27 +1,56 @@
 use std::collections::HashMap;
 use std::boxed::Box;
 
-trait Connection {
+pub trait Connection<'a> {
+  //fn new () -> Self;
   fn is_open (self) -> bool;
-  fn communication_method (self) -> dyn CommunicationMethod;
+  fn communication_method (self) -> dyn CommunicationMethod<'a>;
 }
 
 
-pub trait CommunicationMethod {
-  fn name (self) -> String;
-  fn connect (self) -> dyn Connection;
-  fn set_core (&mut self, core: &DeviceCore);
+pub trait CommunicationMethod <'a> {
+  fn name (&self) -> &str;
+  fn connect (&self) -> dyn Connection;
+  fn set_core (&mut self, core: DeviceCore<'a>);
 }
 
 
-pub struct DeviceCore {
-  connections: Vec<Box<dyn Connection>>,
-  communication_methods:  HashMap<String, Box<dyn CommunicationMethod>>
+pub struct DeviceCore<'a> {
+  connections: Vec<Box<dyn Connection<'a>>>,
+  communication_methods:  HashMap<Box<&'a str>, Box<&'a dyn CommunicationMethod<'a>>>
 }
 
-impl DeviceCore {
-  pub fn add_communication_method (&mut self, method: Box<dyn CommunicationMethod>) -> &mut DeviceCore {
-    self.communication_methods.insert(String::from(*method.name()), method);
+impl <'a> DeviceCore <'a> {
+  pub fn add_communication_method (&mut self, method: Box<&'a dyn CommunicationMethod<'a>>) -> &mut Self {
+    self.communication_methods.insert(Box::from(method.name()), method);
     self
   }
+}
+
+
+// test impl
+
+struct Test <'a> {
+  communication_name: String,
+  device_core: DeviceCore<'a>
+}
+
+impl<'a> CommunicationMethod<'a> for Test<'a> {
+  fn name (&self) -> &str {
+    &self.communication_name
+  }
+  fn set_core(&mut self, core: DeviceCore<'a>) {
+    self.device_core = core;
+  }
+
+  fn connect (&self) -> TestConnection {
+
+  }
+}
+
+struct TestConnection {
+}
+
+impl <'a> Connection<'a> for TestConnection {
+
 }
