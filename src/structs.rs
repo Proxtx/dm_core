bf17f76cedd3b38c9e27d376fdb84c752182c638
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-06-26 19:32:52 +0200

    discover on communicationMethod

--- src/structs.rs
+++ src/structs.rs
@@ -2,17 +2,15 @@ use std::collections::HashMap;
 use std::boxed::Box;
 
 pub trait Connection<'a> {
-  //fn new () -> Self;
   fn is_open (&self) -> bool;
 }
 
-
 pub trait CommunicationMethod <'a> {
   fn name (&self) -> &str;
-  fn connect (&self) -> Box<dyn Connection>;
+  fn connect (&self, id: &str) -> Box<dyn Connection>;
   fn set_core (&mut self, core: DeviceCore<'a>);
+  fn discover (&self) -> Vec<&str>;
 }
-
 
 pub struct DeviceCore<'a> {
   connections: Vec<Box<dyn Connection<'a>>>,
@@ -42,10 +40,14 @@ impl<'a> CommunicationMethod<'a> for Test<'a> {
     self.device_core = Option::from(core);
   }
 
-  fn connect (&self) -> Box<dyn Connection<'_>> {
+  fn connect (&self, id: &str) -> Box<dyn Connection<'_>> {
     Box::from(TestConnection {
 
     })
+  }
+
+  fn discover(&self) -> Vec<&str> {
+    vec!["gfhjdhalidf"]
   }
 }
 
